9c3a0ab377fb418ead8907ea8900aacaadcd50e5
rps278 <31069347+rps278@users.noreply.github.com>
2019-01-27 23:41:06 -0500

    Update TestMarketPlace.sol

--- test/TestMarketPlace.sol
+++ test/TestMarketPlace.sol
@@ -9,21 +9,20 @@ contract TestMarketPlace {
 
   MarketPlace mplace = MarketPlace(DeployedAddresses.MarketPlace());
 
+  
+   // Purpose of this test is to test the value of the skuCount variable
   function testSKUCount() public{
     Assert.equal(mplace.skuCount(),0,"SKU Count is 0");
   }
 
+     // Purpose of this test is to test the value of the Customer Count variable
   function testCustomerCount() public {
     Assert.equal(mplace.customerCount(),0,"Customer Count should be 0");
   }
 
+     // Purpose of this test is to test the value of the skuCount variable
   function testStoreOwnerCount() public {
     Assert.equal(mplace.storeOwnerCount(),0,"StoreOwner Count should be 0");
-  }
-
-  function testSKUToReceivedState() public {
-        Assert.equal(mplace.customerCount(),0,"Customer Count should be 0");
-
   }
 
 }
